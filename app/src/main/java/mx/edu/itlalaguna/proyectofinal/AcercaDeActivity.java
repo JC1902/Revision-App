1d64b8f6c6fb612321ab0460d08dd4b05c9e06cb
Jorge Cisneros de la Torre <jorgecisneros2002@gmail.com>
2023-11-29 10:43:27 -0600

    Estilos, sujetos a cambios

--- app/src/main/java/mx/edu/itlalaguna/proyectofinal/AcercaDeActivity.java
+++ app/src/main/java/mx/edu/itlalaguna/proyectofinal/AcercaDeActivity.java
@@ -3,6 +3,7 @@ package mx.edu.itlalaguna.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AcercaDeActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AcercaDeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+    }
+
+    public void fabRegresarClick ( View v ) {
+        finish ();
     }
 }
--- app/src/main/java/mx/edu/itlalaguna/proyectofinal/MainActivity.java
+++ app/src/main/java/mx/edu/itlalaguna/proyectofinal/MainActivity.java
@@ -2,7 +2,9 @@ package mx.edu.itlalaguna.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fabAcercaDeClick ( View v ) {
+        Intent intent = new Intent( MainActivity.this, AcercaDeActivity.class );
+        startActivity ( intent );
     }
 }
