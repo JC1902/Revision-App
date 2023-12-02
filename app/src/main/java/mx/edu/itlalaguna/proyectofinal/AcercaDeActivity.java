c861a79379f0a765e71aad0e633875924493a90d
Jorge Cisneros de la Torre <jorgecisneros2002@gmail.com>
2023-12-02 16:16:13 -0600

    Diseño de lista de materias, numeros de control de todos, diseño provisional de splash y recursos para iconos de la lista de materias

--- app/src/main/java/mx/edu/itlalaguna/proyectofinal/AcercaDeActivity.java
+++ app/src/main/java/mx/edu/itlalaguna/proyectofinal/AcercaDeActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class AcercaDeActivity extends AppCompatActivity {
+
+    TextView integrantes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        integrantes = findViewById( R.id.txvEquipo );
+
+        integrantes.append( "\n" + "Jorge Cisneros de la Torre 20130789\n" +
+                            "Carlos Fernando Aguilera Garcia 20130811\n" +
+                            "César Gabriel Zamora Morales 20130849\n" +
+                            "Pedro Sebastián García Favela 20130802\n" +
+                            "Shalom Isai Salazar Arguijo 20130805" );
+
     }
 
     public void fabRegresarClick ( View v ) {
