13b5c6a1b8d35be4e012ef88a2daa599fd000bff
Labrielt <cesargabrielzamoramorale@gmail.com>
2023-12-06 19:57:10 -0600

    Arreglo del lista_tareas.xml

--- app/src/main/java/mx/edu/itlalaguna/proyectofinal/Listas/Tareas/ItemModelTarea.java
+++ app/src/main/java/mx/edu/itlalaguna/proyectofinal/Listas/Tareas/ItemModelTarea.java
@@ -1,22 +1,22 @@
-package mx.edu.itlalaguna.proyectofinal.Listas.Tareas;
-
-import mx.edu.itlalaguna.proyectofinal.R;
-
-public class ItemModelTarea {
-
-    String nombre;
-    static int imagen = R.drawable.tarealist;
-
-
-    public ItemModelTarea ( String nombre ) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre () {
-        return nombre;
-    }
-
-    public static int getImagen () {
-        return imagen;
-    }
-}
+//package mx.edu.itlalaguna.proyectofinal.Listas.Tareas;
+//
+//import mx.edu.itlalaguna.proyectofinal.R;
+//
+//public class ItemModelTarea {
+//
+//    String nombre;
+//    static int imagen = R.drawable.tarealist;
+//
+//
+//    public ItemModelTarea ( String nombre ) {
+//        this.nombre = nombre;
+//    }
+//
+//    public String getNombre () {
+//        return nombre;
+//    }
+//
+//    public static int getImagen () {
+//        return imagen;
+//    }
+//}
