8cf189a4a6787539659b4bb5dc459074d59b95be
Shalom <shalomsalazar@2002@gmail.com>
2023-12-10 21:10:39 -0600

    revisa tareas en tiempo real

--- app/src/main/java/mx/edu/itlalaguna/proyectofinal/TareaActivity.java
+++ app/src/main/java/mx/edu/itlalaguna/proyectofinal/TareaActivity.java
@@ -28,225 +28,225 @@ public class TareaActivity extends AppCompatActivity {
 
     MenuItem filtro;
     Boolean pendientes = true;
-    List<String> listaString;
-    ArrayAdapter<String> arrayAdapter;
-    private final String [] alumnosListos   = { "Alumno 20" ,  "Alumno 43" ,  "Alumno 52" , "Alumno 6" , "Alumno 1" ,};
+    List < String > listaString;
+    ArrayAdapter < String > arrayAdapter;
+    private final String[] alumnosListos = { "Alumno 20", "Alumno 43", "Alumno 52", "Alumno 6", "Alumno 1", };
 
-    private final String [] alumnosPendientes   = { "Alumno 2" ,  "Alumno 4" ,  "Alumno 12" , "Alumno 23" , "Alumno 11" ,};
+    private final String[] alumnosPendientes = { "Alumno 2", "Alumno 4", "Alumno 12", "Alumno 23", "Alumno 11", };
     private ListView listaAlumnos;
-    private  ArrayList < String > alumnos;
+    private ArrayList < String > alumnosControl;
+    private ArrayList < String > alumnos;
     private BaseDatosHelper dbHelper;
     private String idTarea;
     private String nombreTarea;
-    private ArrayList < String > alumnosL ;
-    private ArrayList < String > alumnosP ;
-    private ArrayList < String > alumnosV ;
+    private ArrayList < String > alumnosL;
+    private ArrayList < String > alumnosP;
+    private ArrayList < String > alumnosV;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tarea);
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_tarea );
 
-        dbHelper = new BaseDatosHelper( this );
-        Intent intent = getIntent();
-        setTitle( intent.getStringExtra("Nombre" ) );
-        Toolbar myToolbar = findViewById(R.id.tb_tarea);
-        setSupportActionBar(myToolbar);
+        dbHelper = new BaseDatosHelper ( this );
+        Intent intent = getIntent ( );
+        setTitle ( intent.getStringExtra ( "Nombre" ) );
+        Toolbar myToolbar = findViewById ( R.id.tb_tarea );
+        setSupportActionBar ( myToolbar );
 
-        idTarea = intent.getStringExtra( "IdTarea" );
-        nombreTarea = intent.getStringExtra( "Nombre" );
+        idTarea = intent.getStringExtra ( "IdTarea" );
+        nombreTarea = intent.getStringExtra ( "Nombre" );
+        listaAlumnos = findViewById ( R.id.lvTareasAlumnos );
+        cargarAlumnos ( );
 
-    //        listaTareas = findViewById ( R.id.lvTareasAlumnos);
-    //        alumnos = alumnosPendientes;
-    //
-    //        listaString =new ArrayList<>(Arrays.asList( alumnos ));
-    //        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice , android.R.id.text1, alumnos);
-    //        listaTareas.setAdapter(arrayAdapter);
-
-        // Asegúrate de que listaTareas se inicialice correctamente
-        listaAlumnos = findViewById( R.id.lvTareasAlumnos );
-        alumnosP = new ArrayList<>();
-        alumnosL = new ArrayList<>();
-        alumnos = new ArrayList <> ( );
-
-        Cursor cursorPendientes = dbHelper.getTareaAlumnos( idTarea );
-
-        // Recorrer el cursor y clasificar las tareas
-        if (cursorPendientes != null && cursorPendientes.moveToFirst()) {
-            do {
-                String no_Control = cursorPendientes.getString( 0 );
-                String nombreAl = cursorPendientes.getString( 1 ) + " " + cursorPendientes.getString( 2 );
-                int hecha = cursorPendientes.getInt(3);
-                String alumno = nombreAl + " - " + no_Control;
-
-                alumnos.add ( alumno );
-                if (hecha == 0) {
-                    alumnosP.add( alumno );
-                } else {
-                    alumnosL.add( alumno );
-                }
-            } while (cursorPendientes.moveToNext());
-
-            cursorPendientes.close();
-        }
-
-        // Inicializar lista
-        alumnosV = alumnosP;
-        listaString = new ArrayList<>( alumnosV );
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, android.R.id.text1,alumnosV );
-        listaAlumnos.setAdapter(arrayAdapter);
 
     }
+
+    private void cargarAlumnos ( ) {
+        alumnosP = new ArrayList <> ( );
+        alumnosL = new ArrayList <> ( );
+        alumnos = new ArrayList <> ( );
+        alumnosControl=new ArrayList <> ( );
+
+        Cursor cursorPendientes = dbHelper.getTareaAlumnos ( idTarea );
+
+        // Recorrer el cursor y clasificar las tareas
+        if ( cursorPendientes != null && cursorPendientes.moveToFirst ( ) ) {
+            do {
+                String no_Control = cursorPendientes.getString ( 0 );
+                String nombreAl = cursorPendientes.getString ( 1 ) + " " + cursorPendientes.getString ( 2 );
+                int hecha = cursorPendientes.getInt ( 3 );
+                String alumno = nombreAl + " - " + no_Control;
+                alumnosControl.add ( no_Control );
+                alumnos.add ( alumno );
+                if ( hecha == 0 ) {
+                    alumnosP.add ( alumno );
+                } else {
+                    alumnosL.add ( alumno );
+                }
+            } while ( cursorPendientes.moveToNext ( ) );
+
+            cursorPendientes.close ( );
+        }
+        // Inicializar lista
+        alumnosV = alumnosP;
+        listaString = new ArrayList <> ( alumnosV );
+        arrayAdapter = new ArrayAdapter <> ( this, android.R.layout.simple_list_item_multiple_choice, android.R.id.text1, alumnosV );
+        listaAlumnos.setAdapter ( arrayAdapter );
+    }
+
     //----------------------------------------------------------------------------------------------
     public boolean onCreateOptionsMenu ( Menu menu ) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_searchview_filtro, menu);
+        MenuInflater inflater = getMenuInflater ( );
+        inflater.inflate ( R.menu.menu_searchview_filtro, menu );
 
 
-        MenuItem buscador = menu.findItem(R.id.search_view);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(buscador);
+        MenuItem buscador = menu.findItem ( R.id.search_view );
+        SearchView searchView = ( SearchView ) MenuItemCompat.getActionView ( buscador );
 
-        filtro = menu.findItem(R.id.filtro);
+        filtro = menu.findItem ( R.id.filtro );
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchView.setOnQueryTextListener ( new SearchView.OnQueryTextListener ( ) {
             @Override
-            public boolean onQueryTextSubmit(String query) {
+            public boolean onQueryTextSubmit ( String query ) {
                 return false;
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
-                TareaActivity.this.arrayAdapter.getFilter().filter(newText);
+            public boolean onQueryTextChange ( String newText ) {
+                TareaActivity.this.arrayAdapter.getFilter ( ).filter ( newText );
                 return false;
             }
-        });
+        } );
 
         return true;
     }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.filtro) {
+    public boolean onOptionsItemSelected ( MenuItem item ) {
+        int id = item.getItemId ( );
+        if ( id == R.id.filtro ) {
+            actualizarTareasEnBaseDeDatos ();
+            cargarAlumnos ();
             pendientes = !pendientes;
-            if( pendientes ){
+            if ( pendientes ) {
                 filtro.setIcon ( R.drawable.pendiente );
                 alumnosV = alumnosP;
-                seleccionados();
-                actualizarLista();
+                seleccionados ( );
+                actualizarLista ( );
 
             } else {
                 filtro.setIcon ( R.drawable.revisados );
                 alumnosV = alumnosL;
-                seleccionados();
-                actualizarLista();
+                seleccionados ( );
+                actualizarLista ( );
 
-                for (int i = 0; i < arrayAdapter.getCount(); i++) {
-                    listaAlumnos.setItemChecked(i, true);
+                for ( int i = 0 ; i < arrayAdapter.getCount ( ) ; i++ ) {
+                    listaAlumnos.setItemChecked ( i, true );
                 }
 
             }
 
-        }else
-            return super.onOptionsItemSelected(item);
-
+        } else
+            return super.onOptionsItemSelected ( item );
 
 
         return true;
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onPause ( ) {
+        super.onPause ( );
 
         // Realizar la actualización de la base de datos con las tareas pendientes
-        if (pendientes) {
-            actualizarTareasEnBaseDeDatos();
+        if ( pendientes ) {
+            actualizarTareasEnBaseDeDatos ( );
         }
     }
-    private void actualizarTareasEnBaseDeDatos() {
+
+    private void actualizarTareasEnBaseDeDatos ( ) {
         // Aquí debes implementar la lógica para actualizar las tareas en la base de datos
 
-        for (int i = 0; i < listaAlumnos.getCount(); i++) {
+        int idTarea = Integer.parseInt ( getIntent ( ).getStringExtra ( "IdTarea" ) );
+
+        for ( int i = 0 ; i < listaAlumnos.getCount ( ) ; i++ ) {
             // Obtener el nombre de la tarea
-            int idT = Integer.parseInt( idTarea );
-            String alumnos = (String) listaAlumnos.getItemAtPosition(i);
-            String[] partes = alumnos.split(" - ");
-            String numControl = partes[ 1 ];
+            String numControl = alumnosControl.get ( i );
 
             // Verificar si la tarea está marcada en el ListView
-            boolean tareaMarcada = listaAlumnos.isItemChecked(i);
+            boolean tareaMarcada = listaAlumnos.isItemChecked ( i );
 
             // Obtener el estado actual de la tarea en la base de datos
             int estadoActual = tareaMarcada ? 1 : 0;
 
             // Actualizar la tarea en la base de datos con el estado actual
-            dbHelper.updateAlumnoTarea(estadoActual, numControl,idT );
+            dbHelper.updateAlumnoTarea ( estadoActual, numControl, idTarea );
         }
     }
 
-    public void actualizarLista (  ) {
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice , android.R.id.text1, alumnosV);
-        listaAlumnos.setAdapter(arrayAdapter);
+    public void actualizarLista ( ) {
+        arrayAdapter = new ArrayAdapter <> ( this, android.R.layout.simple_list_item_multiple_choice, android.R.id.text1, alumnosV );
+        listaAlumnos.setAdapter ( arrayAdapter );
     }
 
-    public void alertBorrarTarea ( View v){
-        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+    public void alertBorrarTarea ( View v ) {
+        AlertDialog.Builder builder = new AlertDialog.Builder ( this );
 
-        builder.setIcon(R.drawable.itl )
-                .setView( R.layout.alerta_eliminar_tarea)
-                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+        builder.setIcon ( R.drawable.itl )
+                .setView ( R.layout.alerta_eliminar_tarea )
+                .setNegativeButton ( "Cancelar", new DialogInterface.OnClickListener ( ) {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+                    public void onClick ( DialogInterface dialog, int which ) {
+                        dialog.dismiss ( );
                     }
-                })
-                .setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+                } )
+                .setPositiveButton ( "Eliminar", new DialogInterface.OnClickListener ( ) {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        borrarTarea( v );
+                    public void onClick ( DialogInterface dialog, int which ) {
+                        borrarTarea ( v );
                     }
-                })
-                .create()
-                .show();
+                } )
+                .create ( )
+                .show ( );
     }
 
-    public void borrarTarea (View v ){
-        boolean tareaElminada = dbHelper.deleteTarea( idTarea, nombreTarea );
+    public void borrarTarea ( View v ) {
+        boolean tareaElminada = dbHelper.deleteTarea ( idTarea, nombreTarea );
 
         if ( tareaElminada ) {
             // Si la materia se borra correctamente, puedes cerrar la actividad actual
             // Notifica al adaptador que los datos han cambiado
 
-            Intent resultIntent = new Intent(  );
-            setResult( RESULT_OK, resultIntent );
-            finish();
+            Intent resultIntent = new Intent ( );
+            setResult ( RESULT_OK, resultIntent );
+            finish ( );
             // También podrías mostrar un mensaje o realizar otras acciones después de borrar la tarea
         } else {
             // Muestra un mensaje si hay un error al borrar la tarea
-            Toast.makeText(this, "Error al borrar la tarea", Toast.LENGTH_LONG).show();
+            Toast.makeText ( this, "Error al borrar la tarea", Toast.LENGTH_LONG ).show ( );
         }
 
     }
-    public void seleccionados () {
-        Cursor cursorPendientes = dbHelper.getTareaAlumnos( idTarea );
 
-        long [] test = listaAlumnos.getCheckItemIds();
+    public void seleccionados ( ) {
+        Cursor cursorPendientes = dbHelper.getTareaAlumnos ( idTarea );
+
+        long[] test = listaAlumnos.getCheckItemIds ( );
         String elementos = "";
-        for (long i : test) {
-            if (cursorPendientes != null && cursorPendientes.moveToFirst()) {
+        for ( long i : test ) {
+            if ( cursorPendientes != null && cursorPendientes.moveToFirst ( ) ) {
                 do {
-                    String idAlumno = cursorPendientes.getString(0);
-                    int hecha = cursorPendientes.getInt(3);
+                    String idAlumno = cursorPendientes.getString ( 0 );
+                    int hecha = cursorPendientes.getInt ( 3 );
 
                     if ( hecha == 0 ) {
                         //dbHelper.updateAlumnoTarea(1, idAlumno, idTarea);
                     } else {
-                     //   dbHelper.updateAlumnoTarea(0, idAlumno, idTarea);
+                        //   dbHelper.updateAlumnoTarea(0, idAlumno, idTarea);
                     }
-                } while (cursorPendientes.moveToNext());
+                } while ( cursorPendientes.moveToNext ( ) );
 
-                }
+            }
             elementos += " , " + i;
         }
     }
